c263cfa55f068af4d4f88ea7a8829dbe033e30fb
anais-romero <anais.romero281@gmail.com>
2022-08-09 23:06:55 -0500

    Cambio variables

--- menu.pagina.php
+++ menu.pagina.php
@@ -4,50 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <title>Menú</title>
 </head>
 <body>
-    <section class="mb-3">
-  <nav class="navbar navbar-light bg-light">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-        data-mdb-target="#navbarToggleExternalContent5"
-        aria-controls="navbarToggleExternalContent5" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <i class="fas fa-hamburger"></i>
-      </button>
-    </div>
-  </nav>
-  <div class="collapse" id="navbarToggleExternalContent5">
-    <div class="bg-light shadow-3 p-4">
-      <button class="btn btn-link btn-block border-bottom m-0">Link 1</button>
-      <button class="btn btn-link btn-block border-bottom m-0">Link 2</button>
-      <button class="btn btn-link btn-block m-0">Link 3</button>
-    </div>
-  </div>
-</section>
-
-<!--Chevron icon-->
-<section class="mb-3">
-  <nav class="navbar navbar-light bg-light">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-        data-mdb-target="#navbarToggleExternalContent6"
-        aria-controls="navbarToggleExternalContent6" aria-expanded="false"
-        aria-label="Toggle navigation">
-        <i class="fas fa-chevron-circle-down"></i>
-      </button>
-    </div>
-  </nav>
-  <div class="collapse" id="navbarToggleExternalContent6">
-    <div class="bg-light shadow-3 p-4">
-      <button class="btn btn-link btn-block border-bottom m-0">Link 1</button>
-      <button class="btn btn-link btn-block border-bottom m-0">Link 2</button>
-      <button class="btn btn-link btn-block m-0">Link 3</button>
-    </div>
-  </div>
-</section>
 </body>
 </html>
